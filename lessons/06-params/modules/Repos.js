182f19c8fc477182c5f0543d1eb7f36dd0d8968a
Anatoly Babin <babin@factory.mn>
2017-01-08 18:24:49 +0300

    add: try route prop.params

--- lessons/06-params/modules/Repos.js
+++ lessons/06-params/modules/Repos.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 export default React.createClass({
   render() {
-    return <div>Repos</div>
+    return (
+    <div>Repos
+      <ul>
+        <li><Link to="/repos/reactjs/react-router">React Router</Link></li>
+        <li><Link to="/repos/facebook/react">React</Link></li>
+      </ul>
+    </div>)
   }
 })
